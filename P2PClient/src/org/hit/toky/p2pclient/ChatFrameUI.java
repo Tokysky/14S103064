a4d4e72205d4822373ba7e6b9c3732a2a74881c8
Tokysky <1099566961@qq.com>
2015-07-03 09:49:50 +0800

    p2p文件分发系统

---
+++ P2PClient/src/org/hit/toky/p2pclient/ChatFrameUI.java
@@ -0,0 +1,18 @@
+package org.hit.toky.p2pclient;
+
+import java.awt.Container;
+
+import javax.swing.JFrame;
+
+/**
+ * @author 	 tokysky (HIT-CS-ICES) 
+ * @time	  于2015年7月1日上午10:45:55
+ *
+ * @description 
+ **/
+
+public class ChatFrameUI {
+	JFrame frame;
+	Container cp;
+	
+}
